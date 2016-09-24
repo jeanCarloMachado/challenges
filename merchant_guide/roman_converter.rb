6c350a95ee9eaeeb1f3cae353cdb1dd96bb3881b
Jean Carlo Machado <contato@jeancarlomachado.com.br>
2016-09-24 20:42:36 -0300

    renamed converter files for more semantic names

---
+++ merchant_guide/roman_converter.rb
@@ -0,0 +1,86 @@
+
+class Converter
+  MAP = {
+    'I' => 1,
+    'V' => 5,
+    'X' => 10,
+    'L' => 50,
+    'C' => 100,
+    'D' => 500,
+    'M' => 1000
+  }
+
+  def initialize(symbols)
+    @symbols = symbols
+  end
+
+  def getResult
+    assert_no_more_than_tree_times_in_succession
+    assert_no_more_than_single_occurence
+    assert_I_subtracs_only_valid_symbols
+    assert_X_subtracs_only_valid_symbols
+
+    result = 0
+    i = 0
+    @symbols.split("").each do |symbol|
+      value = getValue(symbol)
+
+      if precedes_greater_number?(value, i)
+        result-=value
+      else
+        result+=value
+      end
+
+      i=i+1
+    end
+    result
+  end
+
+  def precedes_greater_number? (value, indice)
+    next_value = @symbols.split("").at(indice+1)
+    next_value = !next_value ? 0 : getValue(next_value)
+    precedes_great_number = next_value > value
+
+    if (precedes_great_number && consecutive_preceed_of_lesser_number(next_value, indice))
+        raise "Only one small-value symbol may be subtracted from any large-value symbol"
+    end
+
+    precedes_great_number
+  end
+
+  def consecutive_preceed_of_lesser_number (next_value, indice)
+      previous_value = @symbols.split("").at(indice-1)
+      return precedes_greater_number if !previous_value
+      previous_value =  getValue(previous_value)
+
+      previous_value < next_value
+  end
+
+  def getValue(symbol)
+    MAP.each do |key, value|
+      return value if key == symbol
+    end
+  end
+
+  def assert_no_more_than_tree_times_in_succession
+    raise "No more than 3 successions symbols" if /(IIII|XXXX|CCCC|MMMM)/ =~ @symbols
+  end
+
+  def assert_no_more_than_single_occurence
+    raise "Symbols can never be repeated" if /(DD|LL|VV)/ =~ @symbols
+  end
+
+  def assert_I_subtracs_only_valid_symbols
+    raise "I can be subtracted from V and X only" if /(IL|IC|ID|IM)/ =~ @symbols
+  end
+
+  def assert_X_subtracs_only_valid_symbols
+    raise "X can be subtracted from L and C only" if /(XM|XD)/ =~ @symbols
+  end
+
+  def self.resolve(symbols)
+    converter = self.new(symbols)
+    converter.getResult()
+  end
+end
+
--- merchant_guide/roman_converter_test.rb
+++ merchant_guide/roman_converter_test.rb
@@ -1,66 +1,5 @@
 require "test/unit"
-
-class Converter
-  MAP = {
-    'I' => 1,
-    'V' => 5,
-    'X' => 10,
-    'L' => 50,
-    'C' => 100,
-    'D' => 500,
-    'M' => 1000
-  }
-
-  def initialize(symbols)
-    @symbols = symbols
-  end
-
-  def getResult
-
-    assert_no_more_than_tree_times_in_succession
-    assert_no_more_than_single_occurence
-    assert_I_subtracs_only_valid_symbols
-
-    result = 0
-    i = 0
-    @symbols.split("").each do |symbol|
-      value = getValue(symbol)
-      precedes_greater_number?(value, i) ?  result-=value : result+=value
-      i=i+1
-    end
-    result
-  end
-
-  def precedes_greater_number? (value, i)
-      nextValue = @symbols.split("").at(i+1)
-      nextValue = !nextValue ? 0 : getValue(nextValue)
-      nextValue > value
-  end
-
-  def getValue(symbol)
-      MAP.each do |key, value|
-        return value if key == symbol
-      end
-  end
-
-  def assert_no_more_than_tree_times_in_succession
-    raise "No more than 3 successions symbols" if /(IIII|XXXX|CCCC|MMMM)/ =~ @symbols
-  end
-
-  def assert_no_more_than_single_occurence
-    raise "Symbols can never be repeated" if /(DD|LL|VV)/ =~ @symbols
-  end
-
-  def assert_I_subtracs_only_valid_symbols
-    raise "I can be subtracted from V and X only" if /(IL|IC|ID|IM)/ =~ @symbols
-  end
-
-  def self.resolve(symbols)
-    converter = self.new(symbols)
-    converter.getResult()
-  end
-
-end
+load "roman_converter.rb"
 
 class ConverterTest < Test::Unit::TestCase
   def test_single_symbols
@@ -102,13 +41,37 @@ class ConverterTest < Test::Unit::TestCase
 
     invalid_symbols = [ 'IL', 'IC', 'ID', 'IM', 'MID', 'DIC' ]
 
-    invalid_symbols.each do |symbol| 
-        assert_raise_message("I can be subtracted from V and X only") { Converter.resolve(symbol) }
+    invalid_symbols.each do |symbol|
+      assert_raise_message("I can be subtracted from V and X only") { Converter.resolve(symbol) }
     end
 
 
     valid_symbols = [ 'IV', 'IX' ]
     valid_symbols.each { |symbol| assert_nothing_raised {Converter.resolve(symbol)}}
+
+  end
+
+
+  def test_X_can_be_subtracted_from_L_and_C_only
+
+    invalid_symbols = [ 'XD', 'XM', ]
+
+    invalid_symbols.each do |symbol|
+      assert_raise_message("X can be subtracted from L and C only") { Converter.resolve(symbol) }
+    end
+
+
+    valid_symbols = [ 'XL', 'XC' ]
+    valid_symbols.each { |symbol| assert_nothing_raised {Converter.resolve(symbol)}}
+  end
+
+
+  def test_only_one_small_value_symbol_may_be_subtracted
+    invalid_symbols = [ 'IIV', 'IVX', 'IIX', 'XXL']
+
+    invalid_symbols.each do |symbol|
+      assert_raise_message("Only one small-value symbol may be subtracted from any large-value symbol") { Converter.resolve(symbol) }
+    end
 
   end
 end
